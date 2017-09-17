78e2596565538c0046946abb69bb30915437f152
super zozo <Anh.Nguyen3@metropolia.fi>
2017-09-17 19:59:37 +0300

    add notification

--- app/redux/store.js
+++ app/redux/store.js
@@ -2,14 +2,14 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
 import { reducer as form } from 'redux-form'
 import thunk from 'redux-thunk';
 
-import { promiseDispatchMiddleWare, promiseFlattenerMiddleWare, socketMiddleware } from './redux-middleware.js';
+import { promiseFlattenerMiddleWare, socketMiddleware, notificationMiddeware } from './redux-middleware.js';
 import rootReducer from './rootReducer';
 import SocketService from '../services/socket-service'
 
 const store = createStore(
   rootReducer,
   compose(
-    applyMiddleware(thunk, promiseDispatchMiddleWare, promiseFlattenerMiddleWare, socketMiddleware),
+    applyMiddleware(thunk, promiseFlattenerMiddleWare, socketMiddleware, notificationMiddeware),
     process.env.NODE_ENV !== 'production' && window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
   )
 )

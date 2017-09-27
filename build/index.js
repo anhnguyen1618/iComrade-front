1bb4d7ac17ac7a815fe6d4d14b7d92a6d83a4054
super zozo <Anh.Nguyen3@metropolia.fi>
2017-09-28 01:19:42 +0300

    add proxy server

--- build/index.js
+++ build/index.js
@@ -1,20 +1,27 @@
 const express = require('express');
 const proxy = require('http-proxy-middleware');
+const path = require('path');
+
+
+const target = process.env.PROXY_TARGET || 'https://icomrade.herokuapp.com/'
 
 var wsProxy = proxy('/', {
-  target: 'https://icomrade.herokuapp.com/',
+  target,
   changeOrigin: true,                     // for vhosted sites, changes host header to match to target's host
   ws: true                               // enable websocket proxy
 })
 
 const app = express();
-app.use('/public', express.static(__dirname + '/public/'))
-app.use('/api', proxy({target: 'http://www.example.org/api', changeOrigin: true}))
-app.use(wsProxy)
 
-app.get('*', (req, res) => {
-	res.send('index.html');
-})
+const renderIndex = (req, res) => res.sendFile(path.join(__dirname + '/index.html'));
+
+// React route
+app.get('/', renderIndex)
+app.get('/dashboard', renderIndex)
+
+app.use('/public', express.static(__dirname + '/public/'))
+app.use('/api', proxy({target, changeOrigin: true}))
+app.use(wsProxy)
 
 var server = app.listen(process.env.PORT || 8000)
 server.on('upgrade', wsProxy.upgrade)
